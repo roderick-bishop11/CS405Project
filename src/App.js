02678a636db859369e93f77999401a686fb5b407
Ramon Lavender <lavenderramon60@gmail.com>
2021-12-01 12:49:16 -0500

    update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
+  const [people, set People] = useState{data}
+ 
   return <h2>reminder project setup</h2>;
+    {people.map((person) => {
+      const { id, name, Picture } = person
+      
+      return {
+        <div className="container" key=(id)>
+          <div className="avatar">
+          <div>
+            img scr={image} alt={name} />
+          </div>
+            
+          <div>
+            <h3>{name}</h3>
+          </div>
+          </div>
+        
+                
 }
 
 export default App;
