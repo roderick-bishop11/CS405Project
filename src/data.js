c0baaab34be9a6565bebaef169d08b7759b644f7
Roderick Bishop <rbbishop11@gmail.com>
2021-12-02 13:31:48 -0500

    final touches to change things up a bit

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export default [
   //we can edit this data and the mapping in list.js to match our person data sa defined in our UML's
   {
     id: 1,
-    name: 'Bertie Yates',
+    name: 'Madea Yates',
     age: 29,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
@@ -12,14 +12,14 @@ export default [
     name: 'Hester Hogan',
     age: 32,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+      'https://source.unsplash.com/user/c_v_r',
   },
   {
     id: 3,
     name: 'Larry Little',
     age: 36,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+      'https://i.picsum.photos/id/1062/5092/3395.jpg?hmac=o9m7qeU51uOLfXvepXcTrk2ZPiSBJEkiiOp-Qvxja-k',
   },
   {
     id: 4,
@@ -31,8 +31,16 @@ export default [
   {
     id: 5,
     name: 'Lola Gardner',
-    age: 29,
+    age: 8,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+      'https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
   },
+  {
+    id: 6,
+    name: 'Amber Johnson',
+    age: 35,
+    image:
+      'https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
+  },
+
 ];
