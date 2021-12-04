8c6ce4f1701948fb6e45f2b08a0062ce13e6a5df
Ramon Lavender <lavenderramon60@gmail.com>
2021-12-04 15:33:14 -0500

    Lav branch (#15)
    * latest version
    
    * Documented
    
    Co-authored-by: Roderick Bishop <rbbishop11@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import data from './data';//Imported data models
+import List from './List';//Imported structure of list
+//Beginning of app
 function App() {
-  const [people, setPerson] = useState(data)
+//Initialize the Variables that need to be tracked using useState()
+  const [people] = useState(data)
+  const [setPerson] = useState(data)
+  //A new value of id is given to the variable from newPerson
+  //newPerson variable is assigned a new person given the current id and the given id
+  //  are not the same value or type
+  
+//   const [people, setPerson] = useState(data)
   const removePerson = (id) => {
     let newPerson = people.filter((person) => person.id !== id)
     setPerson(newPerson)
   }
+  // Here is the HTML that is loaded onto the webpage
+  return (
+  <main>
+    <section className='container'>
+      <h3>{people.length} birthdays today</h3>
+    //Create the carousel
+      <div class="carousel">
+    // added previous button
+        <button class="carousel_prev_btn" id="previous"></button>
+     //vertical list of people in our data model
+        <div class="carousel_images">
+          <List people={people} />
+        </div>
+    // added next button
+        <button class="carousel_next_btn" id="next"></button>
+    // clear all button; on click list is cleared 
+        <button onClick={() => setPeople("")}>clear all</button>
+      </div>
 
+    </section>
+  </main>
+)
+}
+export default App
 
   //commented out code from our original
   // return <h2>reminder project setup</h2>;
@@ -83,12 +114,10 @@ export default App
 
 // this is also a viable option, We'd need to edit the CSS tho.
 // const [people, setPeople] = useState(data)
-
 // const removePerson = (id) => {
 //   let newPerson = people.filter((person) => person.id !== id)
 //   setPeople(newPerson)
 // }
-
 // return (
 //   <>
 //     <h3
