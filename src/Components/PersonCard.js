8f92d84898b1d620e3ff37b0a35ebf48642330aa
Roderick Bishop <rbbishop11@gmail.com>
2022-04-26 01:16:10 -0400

    added personCard and a carousel

--- src/Components/PersonCard.js
+++ src/Components/PersonCard.js
@@ -1,1 +1,26 @@
 //we will build a card for each person here
+
+import { CardHeader } from "@material-ui/core";
+import { Card, CardContent, CardMedia } from "@mui/material";
+import React from "react";
+
+const PersonCard = ({ person }) => {
+	return (
+		<Card>
+			<CardContent>
+				<CardHeader title={`${person.name}, ${person.age}`}/>
+				<CardMedia
+					component="img"
+					height="200"
+					width="200"
+					image={person.image}
+					alt={person.name}
+                    
+				/>
+
+			</CardContent>
+		</Card>
+	);
+};
+
+export default PersonCard;
